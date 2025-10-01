8d11d52fc588533d5f24bec5039904203a46276e
MiguelArfTeixeira <miguel.arf.teixeira@escola.pr.gov.br>
2025-10-01 15:40:55 -0300

    01/10

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-function criaCartao(categoria,pergunta,resposta) {
+function criaCartao(categoria, pergunta, resposta) {
 
-console.log(categoria,pergunta,resposta)
+    console.log(categoria, pergunta, resposta)
 
-let container = document.getElementById('container')
-let cartao = document.createElement('article')
-cartao.className = 'cartao'
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
 
-cartao.innerHTML=`
+    cartao.innerHTML = `
 
-<div class="cartao__conteudo">
+                <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
                         <p>${pergunta}</p>
@@ -20,5 +20,5 @@ cartao.innerHTML=`
                 </div>
 
 `
-container.appendChild(cartao)
+    container.appendChild(cartao)
 }
